dc4af08d1f8a73c6ba2b64f0a996729d9d5366c1
ahmadmusacs <ahmadmusacs@gmail.com>
2020-12-24 09:39:15 -0700

    Version1_working code_pushed pom, App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,55 +12,37 @@ import org.mockserver.client.MockServerClient;
 import static org.mockserver.model.Cookie.cookie;
 import static org.mockserver.model.HttpRequest.request;
 import static org.mockserver.model.HttpResponse.response;
-import static org.mockserver.model.OpenAPIDefinition.openAPI;
+// import static org.mockserver.model.OpenAPIDefinition.openAPI;
 import static org.mockserver.model.Parameter.param;
-
+import org.mockserver.logging.*;
+import org.mockserver.model.HttpRequest;
+import org.mockserver.model.HttpResponse;
+import org.mockserver.matchers.TimeToLive;
+import org.mockserver.matchers.Times;
 
 public class App 
 {
 	private static ClientAndServer proxy;
-	private static ClientAndServer mockServer;
+	private static MockServerClient mockServerClient;
 
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
 
-        //mockServer = ClientAndServer.startClientAndServer(1080);
-    }
-
-    public static void startProxy() {
-        proxy = ClientAndServer.startClientAndServer();
-        System.setProperty("http.proxyHost", "127.0.0.1");
-        System.setProperty("http.proxyPort", String.valueOf(proxy.getPort()));
-        mockServer = startClientAndServer();
-        System.setProperty("myService.port", String.valueOf(mockServer.getPort()));
-    }
-
-    public static void stopProxy() {
-        stopQuietly(mockServer);
-        stopQuietly(proxy);
-        System.clearProperty("http.proxyHost");
-        System.clearProperty("http.proxyPort");
-    }
-
-    public void createExpectationMockServerClient() {
-        new MockServerClient("localhost", 1080)
+        proxy = ClientAndServer.startClientAndServer(8081);
+        mockServerClient = new MockServerClient("127.0.0.1", 8081);
+        mockServerClient
             .when(
-                request()
-                    .withMethod("GET")
-                    .withPath("/view/cart")
-                    .withCookies(
-                        cookie("session", "4930456C-C718-476F-971F-CB8E047AB349")
-                    )
-                    .withQueryStringParameters(
-                        param("cartId", "055CA455-1DF7-45BB-8535-4F83E7266092")
-                    )
-            )
+                    HttpRequest.request()
+                            .withMethod("GET")
+                            .withPath("/api/hello"),
+                    Times.unlimited(),
+                    TimeToLive.unlimited())
             .respond(
-                response()
-                    .withBody("some_response_body")
+                    HttpResponse.response()
+                            .withBody("Hello Musa")
+                            .withStatusCode(200)
             );
     }
-
 
 }
